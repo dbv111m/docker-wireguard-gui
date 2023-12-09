6d9282563a3086fcd686bb0b2a39232b347adeec
dbv111m <dbv111m@gmail.com>
2023-12-10 00:47:00 +0300

    3

--- prepare.sh
+++ prepare.sh
@@ -4,8 +4,26 @@
 
 #  chmod +x ./prepare
 
-sudo apt update
-sudo apt upgrade
+sudo apt update -y
+sudo apt upgrade -y
 
+apt install curl -y
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
+
+docker run -d \
+  --name=wg-easy \
+  -e WG_HOST=🚨YOUR_SERVER_IP \
+  -e PASSWORD=🚨YOUR_ADMIN_PASSWORD \
+  -v ~/.wg-easy:/etc/wireguard \
+  -p 51820:51820/udp \
+  -p 51821:51821/tcp \
+  --cap-add=NET_ADMIN \
+  --cap-add=SYS_MODULE \
+  --sysctl="net.ipv4.conf.all.src_valid_mark=1" \
+  --sysctl="net.ipv4.ip_forward=1" \
+  --restart unless-stopped \
+  weejewel/wg-easy
+
+  https://ingeniare.space/it/legkaja-ustanovka-vpn/
+  
